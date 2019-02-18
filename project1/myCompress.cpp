ecba0d90ee3ad6e4548b42f9f8b21c27602d0408
Jacob J <jacobjulagay@gmail.com>
2019-02-18 09:05:57 -0800

    Checking if input file is open

--- project1/myCompress.cpp
+++ project1/myCompress.cpp
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
+ifstream input; // file input
+ofstream output; // file output
+
 int main(int argc, char *argv[]){
 
-    cout << "Test";
+    //Open source file
+    input.open("source.txt");
+    
+    //Check to see if input file can't be opened
+    if(input.fail()){
+        cerr << "Can't open tabledata\n";
+        return 1;
+    }
 
-return 0;
+    //Open desitination file
+    output.open("destination.txt");
+
+    // Read data from txt file
+    string buffer;
+    while(input.is_open()){ // Checking if file is open - returns boolean
+
+        cout << "test";
+
+        //Close file 
+        input.close();
+    }
 }
